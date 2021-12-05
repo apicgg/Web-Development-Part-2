86926f7d15ef505ca5dec457174870df3c5c2578
Anurag Pramanik <p.anurag@outlook.com>
2021-12-05 16:23:53 +0530

    Added bmiCalculator

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -17,6 +17,19 @@ app.post('/', function (req, res) {
   res.send('The result is ' + result);
 });
 
+app.get('/bmicalculator', function (req, res) {
+  res.sendFile(__dirname + '/bmiCalculator.html');
+});
+
+app.post('/bmicalculator', function (req, res) {
+  var weight = Number(req.body.w);
+  var height = Number(req.body.h);
+
+  var n = weight / (height * height);
+
+  res.send('Your BMI is ' + n);
+});
+
 app.listen(3000, function () {
   console.log('Server has been started!');
 });
