01dc3c26ef79794f899b13c8e846d59d5f6aaecb
Anurag Pramanik <p.anurag@outlook.com>
2021-12-22 16:53:22 +0530

    Add body-parser and final commit for weather app

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function (req, res) {
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q=Kolkata&appid=c401820d47fc6a51fc00dd54ba35a660&units=metric';
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function (req, res) {
+  // console.log(req.body.cityName);
+  const query = req.body.cityName;
+  const apiKey = 'c401820d47fc6a51fc00dd54ba35a660';
+  const unit = 'metric';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${unit}`;
+
   https.get(url, function (response) {
     console.log(response.statusCode);
 
     response.on('data', function (data) {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
-
       console.log(temp);
 
       const weatherDescription = weatherData.weather[0].description;
@@ -21,7 +32,7 @@ app.get('/', function (req, res) {
       const iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
       res.write(`<p>The weather is ${weatherDescription}</p>`);
-      res.write(`<h1>The temp of Kolkata is ${temp}degree Celcius</h1>`);
+      res.write(`<h1>The temp of ${query} is ${temp}degree Celcius</h1>`);
       res.write(`<img src = ${iconURL}>`);
       res.send();
     });
